55d357951a10ffd81b80cbe15b4cf0821ac67858
prothegee <prothegee@proton.me>
2026-02-20 12:36:18 +0700

    adding: - cxx standard to 23 - build shared libs default off

--- dbuild.sh
+++ dbuild.sh
@@ -2,7 +2,9 @@
 set -e;
 
 cmake -S . -B build/debug -G "Ninja" \
+    -DCMAKE_CXX_STANDARD=23 \
     -DCMAKE_BUILD_TYPE=Debug \
+    -DBUILD_SHARED_LIBS=OFF \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1;
 
 ln -nsf build/debug/compile_commands.json ./;
