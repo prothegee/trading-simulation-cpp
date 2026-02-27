d0309dbbc3d79081adf0279dc56e468a54fb7996
prothegee <prothegee@proton.me>
2026-02-27 15:20:28 +0700

    adding generate number

--- inc/tradesim/utility_func.hh
+++ inc/tradesim/utility_func.hh
@@ -1,12 +1,16 @@
 #ifndef TRADESIM_UTILITY_FUNC_HH
 #define TRADESIM_UTILITY_FUNC_HH
 #include <expected>
+#include <random>
 #include <string>
 
 #include "config.hh"
 
 namespace tradesim {
 namespace utility {
+
+static std::random_device _rand;
+static std::default_random_engine _rande(_rand());
 
 // --------------------------------------------------------- //
 // find*
@@ -23,6 +27,10 @@ Result_t find_and_replace_all(std::string& source, const std::string& query,
 
 // --------------------------------------------------------- //
 // gen*
+
+template <typename T>
+std::expected<T, std::string> generate_random_number(const T& min,
+                                                     const T& max);
 
 // --------------------------------------------------------- //
 // read*
